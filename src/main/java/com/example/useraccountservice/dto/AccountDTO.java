fd6d011962f6e25c2f2f84d078b1f1ba0bd52bd5
Mustofa Adny <adniadni04@gmail.com>
2026-06-09 11:32:18 +0700

    feat: DTO for user account service

---
+++ src/main/java/com/example/useraccountservice/dto/AccountDTO.java
@@ -0,0 +1,37 @@
+package com.example.useraccountservice.dto;
+
+import com.example.useraccountservice.enums.AccountStatus;
+import com.example.useraccountservice.enums.AccountType;
+import com.example.useraccountservice.enums.Currency;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AccountDTO {
+
+    private Long id;
+
+    private String accountNumber;
+
+    private BigDecimal balance;
+
+    private Currency currency;
+
+    private AccountType accountType;
+
+    private AccountStatus accountStatus;
+
+    private UserDTO user;
+
+    private LocalDateTime createdAt;
+}
