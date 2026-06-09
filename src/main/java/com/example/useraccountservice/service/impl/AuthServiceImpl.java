1883f59a010d40b38bf402ba2f1eea1a22a35fd9
Mustofa Adny <adniadni04@gmail.com>
2026-06-09 15:03:27 +0700

    feat: finish register method

---
+++ src/main/java/com/example/useraccountservice/service/impl/AuthServiceImpl.java
@@ -0,0 +1,81 @@
+package com.example.useraccountservice.service.impl;
+
+import com.example.useraccountservice.dto.*;
+import com.example.useraccountservice.entity.Role;
+import com.example.useraccountservice.entity.User;
+import com.example.useraccountservice.exceptions.BadRequestException;
+import com.example.useraccountservice.exceptions.NotFoundException;
+import com.example.useraccountservice.repository.AccountRepository;
+import com.example.useraccountservice.repository.RoleRepository;
+import com.example.useraccountservice.repository.UserRepository;
+import com.example.useraccountservice.security.JwtService;
+import com.example.useraccountservice.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public ApiResponse<AuthResponse> registerUser(RegistrationRequest registrationRequest) {
+        log.info("We are inside the register user service");
+
+        if(userRepository.existsByEmail(registrationRequest.getEmail())) {
+            throw new BadRequestException("Account already exist for this email");
+        }
+
+        Set<Role> roles = new HashSet<>();
+
+        String roleName = (registrationRequest.getRole() != null && !registrationRequest.getRole().isBlank())
+                ? registrationRequest.getRole().toLowerCase()
+                : "CUSTOMER";
+
+        Role databaseRole = roleRepository.findByName(roleName)
+                .orElseThrow(() -> new NotFoundException("Role with name" + roleName + "Not found"));
+
+        roles.add(databaseRole);
+
+        User userToSave = User.builder()
+                .email(registrationRequest.getEmail())
+                .password(passwordEncoder.encode(registrationRequest.getPassword()))
+                .firstName(registrationRequest.getFirstName())
+                .lastName(registrationRequest.getLastName())
+                .enabled(true)
+                .roles(roles)
+                .build();
+
+        User savedUser = userRepository.save(userToSave);
+
+        String token = jwtService.generateToken(savedUser.getEmail());
+
+        UserDTO userDTO = modelMapper.map(savedUser, UserDTO.class);
+
+        AuthResponse authResponse = AuthResponse.builder()
+                .token(token)
+                .user(userDTO)
+                .build();
+
+        return  new ApiResponse<>(HttpStatus.CONTINUE.value(), "User account created successfully", authResponse);
+    }
+
+    @Override
+    public ApiResponse<AuthResponse> loginUser(LoginRequest loginRequest) {
+        return null;
+    }
+}
