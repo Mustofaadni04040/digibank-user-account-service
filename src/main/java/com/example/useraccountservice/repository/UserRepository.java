e2dd8129ddb3e24f88cc6ea9578c76766d81a346
Mustofa Adny <adniadni04@gmail.com>
2026-06-09 11:11:27 +0700

    feat: repository for user account service

---
+++ src/main/java/com/example/useraccountservice/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.example.useraccountservice.repository;
+
+import com.example.useraccountservice.entity.Role;
+import com.example.useraccountservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+    long countByEnabledTrue();
+
+    List<User> findByEnabledTrue();
+
+    @Query("SELECT COUNT(DISTINCT U) FROM User u JOIN u.roles r WHERE r.name = :roleName")
+    long countByRoleName(@Param("roleName") String roleName);
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE  r = :role")
+    List<User> findByRolesContaining(@Param("role") Role role);
+}
