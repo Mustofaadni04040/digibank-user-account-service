da2f9eb239efa6c0bf226242570e91a444277018
Mustofa Adny <adniadni04@gmail.com>
2026-06-09 14:39:21 +0700

    feat: create model mapper user service

---
+++ src/main/java/com/example/useraccountservice/config/ModelMapperConfig.java
@@ -0,0 +1,25 @@
+package com.example.useraccountservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+//    Entity -> DTO
+//    atau
+//    Request DTO -> Entity
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+
+        return modelMapper;
+    }
+}
